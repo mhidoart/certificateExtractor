9ba283006efafd43f913ce274e143ebffc55bb3f
Medhi Assabbane <medhi.assabbane@ensiie.fr>
2020-12-01 14:07:47 +0100

    adding service -> archieve unzip operations in operations/unzipModule.csv

--- unzip_module.py
+++ unzip_module.py
@@ -9,12 +9,12 @@ import zipfile
 import csv
 import datetime
 
-archiveFile = "visitedUrls.csv"
-def archieveOperation(file_name,absosutePath,isSuccess,motif):
+archiveFile = "operations/unzipModule.csv"
+def archieveOperation(zipfileName,absosutePath,numberOfFiles ,fileNames,isSuccess,motif):
     date=datetime.datetime.now(),
     with open(archiveFile,"a",newline='') as f:
         write = csv.writer(f,delimiter=",")
-        write.writerow([str(file_name),str(absosutePath),str(isSuccess),str(motif),str(date)])
+        write.writerow([str(zipfileName),str(absosutePath),str(numberOfFiles),str(fileNames),str(isSuccess),str(motif),date])
 
 
     
@@ -23,12 +23,13 @@ def unzipFile(file_name,file_path="zipfiles/",destination_path="unzipped/"):
     print("target : ",target)
     if(".zip" not in file_name):
         print("this module only extract zip files")
-        archieveOperation(file_name,target,False,"is not a zip file")
+        archieveOperation(file_name,target,-1,"",False,"is not a zip file")
     else:
         try:
             with zipfile.ZipFile(target,"r") as zip_ref:
                 zip_ref.extractall(os.path.join(destination_path, ""))
-                archieveOperation(file_name,target,True,"success")
+                archieveOperation(file_name,target,len(zip_ref.infolist()),zip_ref.namelist(),True,"success")
+                #print(zip_ref.infolist())
         except Exception as ex :
-            archieveOperation(file_name,target,False,"error while unzipping : " + str(ex))
+            archieveOperation(file_name,target,-1,"",False,"error while unzipping : " + str(ex))
             
